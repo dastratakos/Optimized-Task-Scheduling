0f7aba6fd21c9214cb5e95a812c901b8bbc02318
dastratakos <dastratakos@gmail.com>
2020-09-09 18:27:05 -0700

    Initial commit
    Dean - 11/28/19

---
+++ data_generator.py
@@ -0,0 +1,147 @@
+'''
+Generates data and writes it into a csv file called [FILE_NAME]
+'''
+import sys
+import csv
+import sklearn
+import numpy as np
+import util, math, random, collections
+
+# FILENAME to write to
+FILE_NAME = 'test_data.csv' 
+
+# Data labels
+# 	Request ID: 		Integer 	|| range(0, len(numRequests))
+#	SMT: 				Boolean   	|| [True, False]
+#	Service Requested: 	List(str)   || ['Standard' (3 days), 'Express' (24 hours), 'Speedy' (while you wait)]
+#	Date: 				int 		|| year month day
+#	Time: 				int  		|| hour minute
+DATA_LABELS = ['Request ID', 'SMT', 'Service Requested', 'Date', 'Time']
+
+REQ_ID_INDEX = 0    # Integer     	|| range(0, len(numRequests))
+SMT_BOOLEAN = 1     # Boolean     	|| [True, False]
+SERVICE_INDEX = 2   # List(str)   	|| ['Standard' (3 days), 'Express' (24 hours), 'Speedy' (while you wait)]
+DATE_INDEX = 3      # int  			|| year month day
+TIME_INDEX = 4      # int  			|| hour minute
+
+# Constant values/probabilities
+SMT_FREQ = 0.14487808
+SMT_FREQ_IN_SEASON = 0.3
+SUNDAY_HOURS = 5
+REGULAR_HOURS = 9
+REQUESTS_PER_WEEK = 93.1818
+REQUESTS_PER_HOUR = REQUESTS_PER_WEEK / ((REGULAR_HOURS*6) + (SUNDAY_HOURS*1))
+REGULAR_REQ_WEIGHTS = {'Standard': 0.9, 'Express': 0.09, 'Speedy': 0.01}
+SMT_REQ_WEIGHTS = {'Speedy': 0.1, 'Express': 0.35, 'Standard': 0.55}
+
+DEFAULT_GENERATE_NUMHOURS = 5
+DEFAULT_NUMDAYS = 1
+DEFAULT_STARTDATE = (1, 1)
+DEFAULT_IN_SEASON = False
+CALENDAR_DICT = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+
+
+
+def generateData(numHours, numDays, startDate, inSeason):
+	print('Number of hours of data to generate: ', numHours)
+	print('In season: ', inSeason)
+
+	f = open(FILE_NAME, 'w')
+	w = csv.writer(f)
+	w.writerow(DATA_LABELS)
+
+	smtFreq = SMT_FREQ
+	if inSeason: smtFreq = SMT_FREQ_IN_SEASON
+	print(smtFreq)
+	date = [startDate[0], startDate[1], startDate[2]]
+	reqID_Count = 0
+	for day in range(numDays):
+		reqList = np.random.poisson(REQUESTS_PER_HOUR, numHours)		# reqList is a list where each index (hourNum) contains number of requests during that hour
+		print('------------------------------')
+		print('Day number:', day + 1)
+		print('Request Breakdown: ', reqList)
+		print('Total Number of Requests in %d hours: %d' % (numHours, sum(reqList)))
+
+		date[1] += 1
+		for hour, reqAtHour in enumerate(reqList):
+			for i in range(reqAtHour):
+				# Request ID Index
+				reqID_Count += 1
+
+				# SMT
+				StanfordMensTennis = (random.random() < smtFreq)
+
+				# Service Requested
+				serviceReq = None
+				if StanfordMensTennis: serviceReq = util.weightedRandomChoice(SMT_REQ_WEIGHTS)
+				else: serviceReq = util.weightedRandomChoice(REGULAR_REQ_WEIGHTS)
+
+				# Date
+				if date[1] > CALENDAR_DICT[date[0]]:
+					date[1] = date[1] - CALENDAR_DICT[date[0]]
+					date[0] += 1
+					if date[0] == 13: 
+						date[0] = 1
+						date[2] += 1
+				month = str(date[0])
+				day = str(date[1])
+				year = str(date[2])
+				if len(month) == 1: month = '0' + month
+				if len(day) == 1: day = '0' + day
+				while len(year) < 4: year = '0' + year
+				dateInt = int(year + month + day)
+
+
+				# Time
+				minutes = random.randint(0, 60)
+				minutesStr = str(minutes)
+				hourStr = str(hour)
+				if minutes <= 10: minutesStr = '0' + minutesStr
+				if len(hourStr) == 1: hourStr = '0' + hourStr
+				time = hourStr + minutesStr
+				timeTup = (hour, minutes)
+
+				print('RequestID: %d || SMT: %s || Service Requested: %s || Date: %s || Time: %s' %(reqID_Count, str(StanfordMensTennis), str(serviceReq), str(date[0]) + '/' + str(date[1]) + '/' + str(date[2]), str(timeTup)))
+
+				newData = [reqID_Count, StanfordMensTennis, serviceReq, dateInt, time]
+				w.writerow(newData)
+
+	f.close()
+
+def dateCorrectlyFormatted(dateEntry):
+	date = dateEntry.split('/')
+	print(date)
+	if len(date) == 3:
+		year = date[2]
+		month = date[0]
+		day = date[1]
+		if (not year.isdigit()) or (not month.isdigit()) or (not day.isdigit()): return False, (None, None, None)
+		year = int(year)
+		month = int(month)
+		day = int(day)
+		if CALENDAR_DICT[month] == None or day < 1 or day > CALENDAR_DICT[month]: return False, (None, None, None)
+		return True, (month, day, year)
+	return False, (None, None, None)
+
+
+
+if __name__ == '__main__':
+	print('====GENERATING DATA====')
+	argv = sys.argv[1:]
+	if len(sys.argv) == 0: 
+		generateData(DEFAULT_GENERATE_NUMHOURS, DEFAULT_NUMDAYS, DEFAULT_STARTDATE, DEFAULT_IN_SEASON)
+	elif len(argv) == 1 and argv[0].isdigit(): 
+		generateData(int(argv[0], DEFAULT_NUMDAYS, DEFAULT_STARTDATE, DEFAULT_IN_SEASON))
+	elif len(argv) == 2 and (argv[0].isdigit()) and (argv[1].isdigit()): 
+		generateData(int(argv[0]), int(argv[1]), DEFAULT_STARTDATE, DEFAULT_IN_SEASON)
+	elif len(argv) == 3 and (argv[0].isdigit()) and (argv[1].isdigit()) and (dateCorrectlyFormatted(argv[2])[0]): 
+		generateData(int(argv[0]), int(argv[1]), dateCorrectlyFormatted(argv[2])[1], DEFAULT_IN_SEASON)
+	elif len(argv) >= 4 and (argv[0].isdigit()) and (argv[1].isdigit()) and (dateCorrectlyFormatted(argv[2])[0]) and (argv[3] == 'True' or 'False'): 
+		generateData(int(argv[0]), int(argv[1]), dateCorrectlyFormatted(argv[2])[1], argv[3] == 'True')
+	else:
+		print('Invalid arguments. Arguments must follow the format: python3 data_generator integer True/False')
+	print('====DATA GENERATED====')
+
+    
+    
+    
---
+++ submission.py
@@ -0,0 +1,88 @@
+'''
+File: submission.py
+----------
+This file contains our implementations of value iteration and Q-learning.
+'''
+
+import util, math, random
+from collections import defaultdict
+from util import ValueIteration
+from itertools import combinations
+
+class RacquetsMDP(util.MDP):
+    def __init__(self, numRacquets, numDays, file):
+        """
+        numRacquets: number of racquets that can be string in a day
+        numDays: number of days to consider
+        """
+        self.numRacquets = numRacquets
+        self.numDays = numDays
+        self.data = readFile(file)
+        # TODO: add variable for costs of stringing racquets
+        
+    # file is a string that is the name of the CSV data file
+    # returns a data structure of racquets with their data, grouped by day
+    def readFile(file):
+        # TODO: implement function
+
+    # Empty list of racquets at the start of Day 0
+    def startState(self):
+        return ([], 0)
+
+    # Return a list of strings representing actions possible from |state|.
+    # One action is a tuple of indices that represent picking any 15 (or less) racquets.
+    def actions(self, state):
+        if len(state[0]) < self.numRacquets: # all racquets can be strung for that day
+            return range(len(state[0])) # return list of indices of all racquets
+        # otherwise, there are more racquets to string that can be strung for that day
+        return combinations(range(len(state[0])), self.numRacquets)
+
+    # Given a |state| and |action|, return a list of (newState, prob, reward) tuples
+    # corresponding to the states reachable from |state| when taking |action|.
+    # Remember that if |state| is an end state, you should return an empty list [].
+    def succAndProbReward(self, state, action):
+        if state[1] == self.numDays: return [] # end state
+        
+        # remove racquets based on the action and compute reward of stringing those racquets
+        # (add in probability of customer unsatisfied -> transition probabilities)
+        
+        # add new racquets for next day
+        # (generate new data -> transition probabilities?)
+        
+        return [(1, 0.15, 200), (-1, 0.85, 10)]
+
+    # Set the discount factor (float or integer).
+    def discount(self):
+        return 1
+'''
+############################################################
+# Reference Blackjack Problem 4b: convergence of Q-learning
+# Small test case
+# smallMDP = BlackjackMDP(cardValues=[1, 5], multiplicity=2, threshold=10, peekCost=1)
+smallMDP = RacquetsMDP()
+
+# Large test case
+#largeMDP = BlackjackMDP(cardValues=[1, 3, 5, 8, 10], multiplicity=3, threshold=40, peekCost=1)
+largeMDP = RacquetsMDP()
+
+def simulate_QL_over_MDP(mdp, featureExtractor):
+    # Q-learning
+    mdp.computeStates()
+    qLearn = QLearningAlgorithm(mdp.actions, mdp.discount(), featureExtractor)
+    util.simulate(mdp, qLearn, 30000)
+    
+    # value iteration
+    mdp.computeStates()
+    valueIter = ValueIteration()
+    valueIter.solve(mdp)
+    
+    qLearn.explorationProb = 0
+
+    # compare
+    diff = 0
+    for state in valueIter.pi:
+        if valueIter.pi[state] != qLearn.getAction(state):
+            diff += 1
+    print('Difference:', diff / len(valueIter.pi))
+    # END_YOUR_CODE
+'''
