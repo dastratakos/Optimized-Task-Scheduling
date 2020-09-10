50fc4c9b6fc5d007494363f5d6bf33b0c68001ac
dastratakos <dastratakos@gmail.com>
2020-09-09 18:51:27 -0700

    Add Q-learning class (commented out)
    Dean - 11/30/19

--- submission.py
+++ submission.py
@@ -147,31 +147,77 @@ class RacquetsMDP(util.MDP):
     # Set the discount factor (float or integer).
     def discount(self):
         return 1
-        
-def testMDP():
-    mdp = RacquetsMDP(4, 'test_data_save.csv', 6, 0.10)
-    mdp.computeStates()
-    algorithm = ValueIteration() # implemented for us in util.py
-    algorithm.solve(mdp, .001)
-#    for item in list(algorithm.V): print(item, '--------', algorithm.V[item])
-
-start = timeit.default_timer()
-
-testMDP()
-
-stop = timeit.default_timer()
-print('Time: ', stop - start)
 
 '''
+#############################################################################################################################
+
+# Performs Q-learning.  Read util.RLAlgorithm for more information.
+# actions: a function that takes a state and returns a list of actions.
+# discount: a number between 0 and 1, which determines the discount factor
+# featureExtractor: a function that takes a state and action and returns a list of (feature name, feature value) pairs.
+# explorationProb: the epsilon value indicating how frequently the policy
+# returns a random action
+class QLearningAlgorithm(util.RLAlgorithm):
+    def __init__(self, actions, discount, featureExtractor, explorationProb=0.2):
+        self.actions = actions
+        self.discount = discount
+        self.featureExtractor = featureExtractor
+        self.explorationProb = explorationProb
+        self.weights = defaultdict(float)
+        self.numIters = 0
+
+    # Return the Q function associated with the weights and features
+    def getQ(self, state, action):
+        score = 0
+        for f, v in self.featureExtractor(state, action):
+            score += self.weights[f] * v
+        return score
+
+    # This algorithm will produce an action given a state.
+    # Here we use the epsilon-greedy algorithm: with probability
+    # |explorationProb|, take a random action.
+    def getAction(self, state):
+        self.numIters += 1
+        if random.random() < self.explorationProb:
+            return random.choice(self.actions(state))
+        else:
+            return max((self.getQ(state, action), action) for action in self.actions(state))[1]
+
+    # Call this function to get the step size to update the weights.
+    def getStepSize(self):
+        return 1.0 / math.sqrt(self.numIters)
+
+    # We will call this function with (s, a, r, s'), which you should use to update |weights|.
+    # Note that if s is a terminal state, then s' will be None.  Remember to check for this.
+    # You should update the weights using self.getStepSize(); use
+    # self.getQ() to compute the current estimate of the parameters.
+    def incorporateFeedback(self, state, action, reward, newState):
+        # BEGIN_YOUR_CODE (our solution is 9 lines of code, but don't worry if you deviate from this)
+        target = reward
+        if newState is not None:
+            qOpt = [self.getQ(newState, action) for action in self.actions(newState)]
+            target += self.discount * max(qOpt)
+        prediction = self.getQ(state, action)
+        for name, value in self.featureExtractor(state, action):
+            self.weights[name] -= self.getStepSize() * (prediction - target) * value
+        # END_YOUR_CODE
+
+# Return a single-element list containing a binary (indicator) feature
+# for the existence of the (state, action) pair.  Provides no generalization.
+def identityFeatureExtractor(state, action):
+    featureKey = (state, action)
+    featureValue = 1
+    return [(featureKey, featureValue)]
+
 ############################################################
 # Reference Blackjack Problem 4b: convergence of Q-learning
 # Small test case
 # smallMDP = BlackjackMDP(cardValues=[1, 5], multiplicity=2, threshold=10, peekCost=1)
-smallMDP = RacquetsMDP()
+#smallMDP = RacquetsMDP()
 
 # Large test case
 #largeMDP = BlackjackMDP(cardValues=[1, 3, 5, 8, 10], multiplicity=3, threshold=40, peekCost=1)
-largeMDP = RacquetsMDP()
+#largeMDP = RacquetsMDP()
 
 def simulate_QL_over_MDP(mdp, featureExtractor):
     # Q-learning
@@ -194,3 +240,29 @@ def simulate_QL_over_MDP(mdp, featureExtractor):
     print('Difference:', diff / len(valueIter.pi))
     # END_YOUR_CODE
 '''
+
+
+
+
+def testMDP():
+    mdp = RacquetsMDP(4, 'test_data_save.csv', 6, 0.10)
+    mdp.computeStates()
+    algorithm = ValueIteration() # implemented for us in util.py
+    algorithm.solve(mdp, .001)
+    print('*' * 60)
+    for state in algorithm.pi:
+        print('state:', state)
+        print('\toptimal action:', algorithm.pi[state])
+    '''
+    mdp.computeStates()
+    qLearn = QLearningAlgorithm(mdp.actions, mdp.discount(), identityFeatureExtractor)
+    util.simulate(mdp, qLearn, 30000)
+    '''
+#    for item in list(algorithm.V): print(item, '--------', algorithm.V[item])
+
+start = timeit.default_timer()
+
+testMDP()
+
+stop = timeit.default_timer()
+print('Time: ', stop - start)
